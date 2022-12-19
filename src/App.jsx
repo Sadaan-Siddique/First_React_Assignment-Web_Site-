538782b71ed4f830533b2d10186d0a4af47b0bad
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2022-12-20 00:01:54 +0500

    add_main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import Main from './Components/Main';
 function App() {
   return (
     <>
     <Header/>
+    <Main/>
     </>
   );
 }
